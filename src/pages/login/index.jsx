2bdb5cf506d25ea28e76429a740b5ba16981af48
zantaclaus <tarzuto@gmail.com>
2021-11-06 15:33:39 +0700

    Del booking page and login component.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,37 +1,7 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
-
-const InputForm = () => {
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
-};
 
 const Login = () => {
-  return (
-    <div className="container p-4">
-      <InputForm />
-    </div>
-  );
+  return <h1>Login</h1>;
 };
 
 export default Login;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Redirect, Switch, Route } from "react-router";
-import Booking from "../pages/booking";
 import Home from "../pages/home";
 import Login from "../pages/login";
 import Profile from "../pages/profile";
@@ -13,9 +12,6 @@ const Routes = () => {
       <Switch>
         <Route path="/login">
           <Login />
-        </Route>
-        <Route path="/booking">
-          <Booking />
         </Route>
         <Route path="/profile">
           <Profile />

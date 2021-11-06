37e5c6b935c98de73ee5b7b25e5ceb0ac7713267
zantaclaus <tarzuto@gmail.com>
2021-11-06 15:58:50 +0700

    Create Card component

--- src/pages/rooms/index.jsx
+++ src/pages/rooms/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Card from "../../components/card";
 
 const Rooms = () => {
   return (
     <div>
       <h1>Rooms Page</h1>
+      <Card />
     </div>
   );
 };

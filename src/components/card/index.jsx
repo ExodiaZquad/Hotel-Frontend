a54af0254a6ae75a312b3e99048b4a559a015b81
zantaclaus <tarzuto@gmail.com>
2021-11-08 19:19:45 +0700

    Create Card layout.

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import room from "../../assets/img/room.png";
+import "./card.css";
 
 const Card = () => {
   return (
-    <div>
-      <h1>Card</h1>
+    <div className="card__blocks">
+      <div className="card__header">
+        <img src={room} alt="" className="card__img" />
+      </div>
+      <div className="card__detail">
+        <div className="card__name">Room Supalai A</div>
+        <div className="card__people">5-6</div>
+        <div className="card__price">1000 - 3000</div>
+      </div>
     </div>
   );
 };

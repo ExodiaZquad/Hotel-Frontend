e03d444ddee5d6e376af8272fd22c7314a33ce3f
I AM TON <69200919+Thanakorn255@users.noreply.github.com>
2021-11-15 14:48:02 +0700

    Booking

--- src/components/booking/Booking.jsx
+++ src/components/booking/Booking.jsx
@@ -1,7 +1,185 @@
 import React from "react";
+import "./style.css";
+
+const Room_A = () => (
+  <div class="img-slider">
+      <div class="slide active">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+      <div class="info">
+        <h2>Room A</h2>
+      </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room A</h2>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room A</h2>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room A</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>  
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+  </div>   
+);
+const Room_B = () => (
+  <div class="img-slider">
+      <div class="slide active">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+      <div class="info">
+        <h2>Room B</h2>
+      </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room B</h2>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room B</h2>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>Room B</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+  </div>   
+);
+const House_A = () => (
+  <div class="img-slider">
+      <div class="slide active">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+      <div class="info">
+        <h2>House A</h2>
+      </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House A</h2>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House A</h2>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House A</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+  </div>   
+);
+const House_B = () => (
+  <div class="img-slider">
+      <div class="slide active">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+      <div class="info">
+        <h2>House B</h2>
+      </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House B</h2>
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House B</h2>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
+        </div>
+      </div>
+      <div class="slide">
+        <img src="https://media.discordapp.net/attachments/909012998285307924/909019721582657546/AirForce_HuaHin_10.jpg?width=1014&height=676" alt="" />
+        <div class="info">
+          <h2>House B</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+      </div>
+      <div class="navigation">
+        <div class="btn active"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+        <div class="btn"></div>
+      </div>
+  </div>   
+);
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+var manualNav = function(manual){
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+  });
+
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
 
 const Booking = () => {
-  return <div></div>;
+  return (
+      <div>
+         <div class = 'img-slider'>
+           <Room_A/>
+           <Room_B/>
+           <House_A/>
+           <House_B/>
+         </div>
+      </div>
+
+  );
 };
 
 export default Booking;

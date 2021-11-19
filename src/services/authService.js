a495dda653966ad9ef409a79c50462656f608ddf
zantaclaus <tarzuto@gmail.com>
2021-11-19 23:48:24 +0700

    Imprement Register System.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const apiUrl = "https://hotel-backend-api.herokuapp.com/";
-const apiEndpoint = apiUrl + "api/token/";
+const apiLogin = apiUrl + "api/token/";
+const apiRegister = apiUrl + "api/register/";
 
-export async function login(usernameCheck, passwordCheck) {
+export const login = async (usernameCheck, passwordCheck) => {
   const user_name = usernameCheck;
   const password = passwordCheck;
 
-  const { data: token } = await axios.post(apiEndpoint, {
+  const { data: token } = await axios.post(apiLogin, {
     user_name,
     password,
   });
@@ -15,7 +16,21 @@ export async function login(usernameCheck, passwordCheck) {
   console.log(token);
 
   localStorage.setItem("token", token.access);
-}
+};
+
+export const register = async (data) => {
+  const header = {
+    user_name: data.username,
+    password: data.password,
+    email: data.email,
+    first_name: data.firstname,
+    last_name: data.lastname,
+    tel: data.tel,
+  };
+  console.log(header);
+  const res = await axios.post(apiRegister, header);
+  console.log(res);
+};
 
 export const isAuthen = () => {
   try {
@@ -28,6 +43,7 @@ export const isAuthen = () => {
 
 const exportObject = {
   login,
+  register,
   isAuthen,
 };
 

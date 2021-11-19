6b74f7c4b13cfff6df025834362562c31a655c35
Nuttanan29445 <nuch29445@gmail.com>
2021-11-19 17:01:27 +0700

    loginpage

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,74 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import { FaUserAlt } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { siblings } from "dom-helpers";
+
 const SignIn = () => {
+  const signup__click__signin = () => {
+    const box = document.getElementById("contain");
+    box.classList.toggle("change__page__to__signup");
+  };
+
   return (
-    <div className="signin">
-        <div className="signin__left">
-          <div className="img__shape"></div>
-        </div>
-        <div className="signin__right">
-            <div className="signin__container">
-                <div className="signin__header">Sign In</div>
-                <form action="" className="signin__form">
-                    <div className="input__user"> 
-                      <FaUserAlt className="user__icon"/>
-                      <input type="text" placeholder="Username" />
-                    </div>
-                    <div className="input__pass">
-                      <FaLock className="user__icon"/>
-                      <input type="text" placeholder="Password"/>
-                    </div>
-                </form>
-                <button class="btn__signin" id="sign-in-btn">
-                Sign in
-                </button>
-                <button class="btn__signup" id="sign-up-btn">
-                Sign up
-                </button>
-            </div>
-        </div>
-    </div>
-  );
-};
-const SignUp = () => {
-  return(
-    <div className="signup">
+    <div id="contain" className="login__container">
       <div className="signup__left">
         <div className="signup__container">
-            <div className="signup__header">Sign up</div>
-            <div className="signup__input">
-              <div >
-                <input type="text" placeholder="Firstname" className="signup__first" />
-                <input type="text" placeholder="Lastname" className="signup__last" />
+          <div className="signup__header">Sign up</div>
+          <div className="signup__input">
+            <div className="firstname__lastname">
+              <div className="signup__first">
+                <i>
+                  <FaUserAlt />
+                </i>
+                <input type="text" placeholder="Firstname" />
               </div>
-              <div>
-                <input type="text" placeholder="Email" className="signup__another" />
+              <div className="signup__first">
+                <i>
+                  <FaUserAlt />
+                </i>
+                <input type="text" placeholder="Lastname" />
               </div>
-              <div>
-                <input type="text" placeholder="Telephone" className="signup__another" />
-              </div>
-              <div>
-                <input type="text" placeholder="Username" className="signup__another" />
-              </div>
-              <div>
-                <input type="text" placeholder="Password" className="signup__another" />
-              </div>
-              <button class="btn__signup" id="sign-up-btn">
-                Sign up
-              </button>
             </div>
+            <div className="signup__another">
+              <i>
+                <MdEmail />
+              </i>
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="signup__another">
+              <i>
+                <BsFillTelephoneFill />
+              </i>
+              <input type="text" placeholder="Telephone" />
+            </div>
+            <div className="signup__another">
+              <i>
+                <FaUserAlt />
+              </i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="signup__another">
+              <i>
+                <FaLock />
+              </i>
+              <input type="text" placeholder="Password" />
+            </div>
+            <button class="btn__signup" onClick={signup__click__signin}>
+              Sign up
+            </button>
+          </div>
         </div>
       </div>
-      <div className="signup__right"></div>
+      <div className="login__img">
+        <div className="block__img"></div>
+      </div>
+      <div className="signin__right">
+        <div className="signin__container">
+          <div className="signin__header">Sign In</div>
+          <div className="signin__input">
+            <div className="signin__user">
+              <i>
+                <FaUserAlt />
+              </i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div className="signin__pass">
+              <i>
+                <FaLock />
+              </i>
+              <input type="text" placeholder="Password" />
+            </div>
+            <button class="btn__signin">Sign in</button>
+            <button class="btn__signup" onClick={signup__click__signin}>
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
+
 const Login = () => {
-  return <SignUp/>;
-  // return <SignIn />;
+  return <SignIn />;
 };
 
 export default Login;

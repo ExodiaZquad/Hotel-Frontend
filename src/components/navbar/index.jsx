1c4fe35266c7b9bf7e8676af61f12be7eefb3702
JusAeng <justo_play@hotmail.com>
2021-11-19 22:55:01 +0700

    nav(no response) and slide profile basic

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,32 @@
-import React from 'react'  //rafce
+import {React, useState} from 'react'  //rafce
 import { MenuItems } from './MenuItems'
 import './navbar.css'
+import {Offcanvas, Button} from 'react-bootstrap' 
+
+
+function Example() {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      {/* <Button variant="primary" onClick={handleShow}>
+      </Button> */}
+      <a className="nav_circle" onClick={handleShow}></a>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
 
 const Navbar = () => {
     return (
@@ -8,7 +34,7 @@ const Navbar = () => {
             <ul className="nav_menu">
                 <a className="nav_exodia" href="#">EXODIA</a>
                 <div className="nav_select">
-                    {MenuItems.map((item,index) => {
+                    {/* {MenuItems.map((item,index) => {
                         return (
                             <li key={index} >
                                 <a className={item.cName} href={item.url}>
@@ -16,7 +42,14 @@ const Navbar = () => {
                                 </a>
                             </li>
                         )
-                    })}
+                    })} */}
+                    <li>
+                        <a className="nav_items" href="/home">Home</a>
+                        <a className="nav_items" href="#">Service</a>
+                        <a><Example></Example></a>
+                        <a className="nav_items" href="#">Contact</a>
+                        <a className="nav_items" href="#">Booking</a>
+                    </li>
                 </div> 
                 <a className="nav_login" href="#">LOGIN</a>
             </ul>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,41 +1,13 @@
-import {React, useState} from "react";
+import React from "react";
 import Navbar from "../../components/navbar";
 import './home.css'
-import {Button, Offcanvas} from 'react-bootstrap' 
-
-
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch
-      </Button>
-
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
-        </Offcanvas.Body>
-      </Offcanvas>
-    </>
-  );
-}
 
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        <div className="home_back">AIAIA</div>
-        <Example></Example>
+        <div className="home_back"></div>
     </div>
   );
 };

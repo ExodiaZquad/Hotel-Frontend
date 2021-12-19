f9d247cadb18be5ebbe5835f8bbf15d4ba20110c
zantaclaus <tarzuto@gmail.com>
2021-12-19 19:55:01 +0700

    Change pic1,2,3 in room page.

--- src/pages/room/index.jsx
+++ src/pages/room/index.jsx
@@ -22,17 +22,21 @@ import "./modal.css";
 import { Link } from "react-router-dom";
 
 const MyCarousal = ({ room }) => {
+  const pic1 = window.location.origin + `/rooms/${room.pic1}`;
+  const pic2 = window.location.origin + `/rooms/${room.pic2}`;
+  const pic3 = window.location.origin + `/rooms/${room.pic3}`;
+
   return (
     <Carousel fade variant="dark">
       <Carousel.Item>
-        <img className="d-block w-100" src={room.pic1} alt="First slide" />
+        <img className="d-block w-100" src={pic1} alt="First slide" />
         {/* <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={room.pic2} alt="Second slide" />
+        <img className="d-block w-100" src={pic2} alt="Second slide" />
 
         {/* <Carousel.Caption>
           <h3>Second slide label</h3>
@@ -40,7 +44,7 @@ const MyCarousal = ({ room }) => {
         </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={room.pic3} alt="Third slide" />
+        <img className="d-block w-100" src={pic3} alt="Third slide" />
 
         {/* <Carousel.Caption>
           <h3>Third slide label</h3>
